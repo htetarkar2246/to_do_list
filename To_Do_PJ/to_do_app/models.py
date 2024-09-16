88250c0144433a0e0641f012449727cbc7bff11e
Htet Arkar <115476708+htetarkar2246@users.noreply.github.com>
2024-09-16 08:27:43 +0630

    Delete To_Do_PJ directory

--- To_Do_PJ/to_do_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Todoapp(models.Model):
-    task_description = models.TextField()
-    
-
-
--- To_Do_PJ/to_do_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('list/',views.todo_list, name = 'todo_list'),
-    path('insert_todo',views.insert_todo, name='insert_todo'),
-    path('delete_todo_item/<int:todo_id>',views.delete_todo_item, name = 'delete_todo_item')
-]
